fa715e7fc899fd8b50307be5b85a0f71cddfeb21
Francis Onwumere <fran@mobnia.com>
2021-12-16 11:58:00 +0100

    extend constructor function with readFile and readLine methods

--- SummerText.js
+++ SummerText.js
@@ -2,5 +2,22 @@ function SummerText(file){
   this.fileContents = []
   this.fileName = file
 }
+SummerText.prototype.readFileASync= async function (file){
+  try{
+    const streamedFile = fs.createReadStream(process.cwd()+'/'+file).on('error', (err)=>{
+      console.log('There is no file at supplied path ' + err.path)
+    })
 
+    const lines = this.readFileLines(streamedFile)
+        this.fileContents.push(Number(line) || 0)
+  } catch (err){
+    console.log(err)
+  }
+}
+SummerText.prototype.readFileLines= function (stream){
+  return readline.createInterface({
+    input: stream,
+    crlfDelay: Infinity
+  });
+}
 module.exports = SummerText;
